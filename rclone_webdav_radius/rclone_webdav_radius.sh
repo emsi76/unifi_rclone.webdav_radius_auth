f840de5e61ed747946182682e2ca7f8f433b8f5f
MC76 <michael@cocheteau.de>
2025-06-24 20:15:54 +0200

    Update rclone_webdav_radius.sh

--- rclone_webdav_radius/rclone_webdav_radius.sh
+++ rclone_webdav_radius/rclone_webdav_radius.sh
@@ -2,7 +2,7 @@
 
 # Load environment variables
 set -a
-source /data/rclone/rclone_webdav.env
+source /data/rclone/rclone_webdav_radius.env
 set +a
 
-rclone serve webdav -v "${RCLONE_WEBDAV_ROOT_PATH}" --addr :${RCLONE_WEBDAV_PORT} --key ${RCLONE_WEBDAV_SSL_KEY} --cert ${RCLONE_WEBDAV_SSL_CERT} --htpasswd /data/rclone/htpasswd --log-file=${RCLONE_WEBDAV_LOG_PATH}
+rclone serve webdav --addr :${RCLONE_WEBDAV_PORT} --key ${RCLONE_WEBDAV_SSL_KEY} --cert ${RCLONE_WEBDAV_SSL_CERT} --log-file=${RCLONE_WEBDAV_LOG_PATH} --auth-proxy /data/rclone/auth_proxy.sh
