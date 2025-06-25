9324d63ceecd697d1e712c9f186b396d276b97e8
MC76 <michael@cocheteau.de>
2025-06-25 10:09:02 +0200

    Update auth_proxy.sh

--- rclone_webdav_radius/auth_proxy.sh
+++ rclone_webdav_radius/auth_proxy.sh
@@ -26,7 +26,7 @@ if [ -z "$RCLONE_WEBDAV_RADIUS_USERS" ]; then
     echo "Empty radius allowed list" >> ${RCLONE_WEBDAV_LOG_PATH}
     has_access=1
 else
-    if [ exists_in_list "$RCLONE_WEBDAV_RADIUS_USERS" "," $user ]; then
+    if [ exists_in_list $RCLONE_WEBDAV_RADIUS_USERS "," $user ]; then
         echo "User is in the radius allowed list" >> ${RCLONE_WEBDAV_LOG_PATH}
         has_access=1
     else
