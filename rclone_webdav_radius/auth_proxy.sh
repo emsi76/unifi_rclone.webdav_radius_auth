178efbc6a4ff0b60428dc645e45c9685ae95069c
MC76 <michael@cocheteau.de>
2025-06-25 09:42:24 +0200

    Update auth_proxy.sh

--- rclone_webdav_radius/auth_proxy.sh
+++ rclone_webdav_radius/auth_proxy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function exists_in_list() {
+    LIST=$1
+    DELIMITER=$2
+    VALUE=$3
+    [[ "$LIST" =~ ($DELIMITER|^)$VALUE($DELIMITER|$) ]]
+}
+
 STD_IN=$(</dev/stdin)
 echo "STDIN is $STD_IN" >> ${RCLONE_WEBDAV_LOG_PATH}
 
@@ -12,25 +19,42 @@ user=$(echo $STD_IN | jq --raw-output '.user')
 pass=$(echo $STD_IN | jq --raw-output '.pass')
 
 echo "user $user and pass $pass" >> ${RCLONE_WEBDAV_LOG_PATH}
+has_access=false
 
-myhost="$(hostname -f)"
-myhost+="."
-myhost+="$(hostname -y)"
-
-echo "radius host $myhost" >> ${RCLONE_WEBDAV_LOG_PATH} 
-
-auth=$(radtest $user $pass $myhost $RCLONE_WEBDAV_RADIUS_PORT $RCLONE_WEBDAV_RADIUS_SECRET |  grep -c 'Access-Accept')
-
-if [ $auth == 1 ]; then
-    if [ ! -d "${RCLONE_WEBDAV_ROOT_PATH}/${user}_banned/" ]; then
-        mkdir -p "${RCLONE_WEBDAV_ROOT_PATH}/${user}/"
-        printf "{\"type\":\"local\",\"_root\":\"${RCLONE_WEBDAV_ROOT_PATH}/${user}\",\"user\":\"$user\",\"pass\":\"$pass\"}\n"
+if [ -z "${RCLONE_WEBDAV_RADIUS_USERS}" ]; then
+    echo "Empty radius allowed list" >> ${RCLONE_WEBDAV_LOG_PATH}
+    has_access=true
+else
+    if [ exists_in_list "$RCLONE_WEBDAV_RADIUS_USERS" " " $user ]; then
+        echo "User is in the radius allowed list" >> ${RCLONE_WEBDAV_LOG_PATH}
+        has_access=true
     else
-        printf "Blocked login: auth not successful for user $user"
+        echo "User is not in the radius allowed list" >> ${RCLONE_WEBDAV_LOG_PATH}
+    fi
+fi
+
+if [ "$has_access"=true ]; then
+    myhost="$(hostname -f)"
+    myhost+="."
+    myhost+="$(hostname -y)"
+    
+    echo "radius host $myhost" >> ${RCLONE_WEBDAV_LOG_PATH} 
+    
+    auth=$(radtest $user $pass $myhost $RCLONE_WEBDAV_RADIUS_PORT $RCLONE_WEBDAV_RADIUS_SECRET |  grep -c 'Access-Accept')
+    
+    if [ $auth == 1 ]; then
+        if [ ! -d "${RCLONE_WEBDAV_ROOT_PATH}/${user}_banned/" ]; then
+            mkdir -p "${RCLONE_WEBDAV_ROOT_PATH}/${user}/"
+            printf "{\"type\":\"local\",\"_root\":\"${RCLONE_WEBDAV_ROOT_PATH}/${user}\",\"user\":\"$user\",\"pass\":\"$pass\"}\n"
+        else
+            printf "Blocked login: auth not successful for user $user"
+        fi
+    else
+        if [ -d "${RCLONE_WEBDAV_ROOT_PATH}/${user}/" ]; then
+            mv "${RCLONE_WEBDAV_ROOT_PATH}/${user}/" "${RCLONE_WEBDAV_ROOT_PATH}/${user}_banned/"
+        fi
+        printf "Failed login: auth not successful for user $user"
     fi
 else
-    if [ -d "${RCLONE_WEBDAV_ROOT_PATH}/${user}/" ]; then
-        mv "${RCLONE_WEBDAV_ROOT_PATH}/${user}/" "${RCLONE_WEBDAV_ROOT_PATH}/${user}_banned/"
-    fi
-    printf "Failed login: auth not successful for user $user"
+        printf "Blocked login: user $user is not in radius allowed list"
 fi
